6a7cf26f885c6a6d4e9ba418c574535ece03d5fd
zhenyan121 <3367366583@qq.com>
2026-02-17 12:23:40 +0800

    feat: 初始化配置

--- src/config.ts
+++ src/config.ts
@@ -19,13 +19,13 @@ import { LinkPreset } from "./types/config";
 // 移除i18n导入以避免循环依赖
 
 // 定义站点语言
-const SITE_LANG = "ja"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
-	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	subtitle: "a blog by a lazy man",
+	siteURL: "https://zhenyan121.top/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026-02-17", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
@@ -649,3 +649,4 @@ export const widgetConfigs = {
 	pio: pioConfig,
 	share: shareConfig,
 } as const;
+
